9c374ece5d4383ca973d40f7116854a9a846f303
Rasmus Greve <rasmusgreve@gmail.com>
2013-11-16 14:00:26 +0100

    Added base files and favicon

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<html>
+<head>
+<title>Chords</title>
+<link rel="shortcut icon" href="img/icon.png">
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+<link href="./css/styles.css" rel="stylesheet">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+
+
+<script type="text/javascript" src="./js/script.js"></script>
+</body>
+</html>
