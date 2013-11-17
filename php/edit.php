ba746998c8197638b7f5c227dd027b74e460db75
Rasmus Greve <rasmusgreve@gmail.com>
2013-11-17 21:29:34 +0100

    Designed lyric editing

--- php/edit.php
+++ php/edit.php
@@ -10,20 +10,47 @@ function getContent()
 ?>
 <div class='col-md-12'>
 	<h1><?=($id==-1)?'Create':'Edit'?> song</h1>
-	<form class="form-horizontal" role="form">
+	<form class="" role="form">
 		<div class="form-group">
-			<label for="title" class="col-md-2 control-label">Title</label>
-			<div class="col-md-10">
+			<label for="title" class="control-label">Title</label>
 				<input type="text" class="form-control" id="title" placeholder="Title" name="title">
-			</div>
 		</div>
 		<div class="form-group">
-			<label for="author" class="col-md-2 control-label">Author</label>
-			<div class="col-md-10">
+			<label for="author" class="control-label">Author</label>
 				<input type="text" class="form-control" id="author" placeholder="Author" name="author">
+		</div>
+	<!-- Nav tabs -->
+	<ul class="nav nav-tabs">
+		<li class="active"><a href="#lyrics" data-toggle="tab">Lyrics</a></li>
+		<li><a href="#parts" data-toggle="tab">Parts</a></li>
+		<li><a href="#form" data-toggle="tab">Form</a></li>
+	</ul>
+
+	<div class="tab-content">
+		<div class="tab-pane active" id="lyrics">
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<textarea class="form-control" id="lyric_content" placeholder="Type or paste lyrics here..." rows=10></textarea>
+					<br/>
+					<div class="btn-group lyric-pages">
+					  <a href="#" class="btn btn-primary" data-value="1">1</a>
+					  <a href="#" class="btn btn-default" data-value="2">2</a>
+					  <a href="#" class="btn btn-default" id="lyric-add">+</a>
+					</div>
+					<div class="pull-right">
+					  <a href="#" class="btn btn-primary">Save</a>
+					  <a href="#" class="btn btn-danger">Delete</a>
+					</div>
+				</div>
 			</div>
 		</div>
-	<hr/>
+		<div class="tab-pane" id="parts">Parts</div>
+		<div class="tab-pane" id="form">Form</div>
+	</div>
+	
+	<a href="#" class="btn btn-primary">Done</a>
+	
+	<!--
 	<div class="row">
 		<div class="col-md-9">
 			<div class="edit-song-area">
@@ -52,6 +79,7 @@ function getContent()
 			</div>
 		</div>
 	</div>
+	-->
 	</form>
 	
 </div>
@@ -59,8 +87,13 @@ function getContent()
 }
 function getJavascript(){
 ?>
+<script>
+$("#lyric-add").click(function() {
 
+});
+$("#lyric-add").click(function() {
 
+</script>
 <?php
 }
 ?>
