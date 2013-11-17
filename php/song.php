58e1c02263c3fbc460ddbcd5c47a8065ed607f07
Rasmus Greve <rasmusgreve@gmail.com>
2013-11-17 01:46:00 +0100

    Added template for song.php

---
+++ php/song.php
@@ -0,0 +1,21 @@
+<?php
+//before content
+$query = (isset($_GET['query'])) ? $_GET['query'] : '';
+?>
+
+<?php
+function getContent()
+{
+?>
+Show song id : <?=$_GET['id']?>
+
+
+<?php
+}
+function getJavascript(){
+?>
+
+
+<?php
+}
+?>
