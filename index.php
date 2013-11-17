0efa3a7bb17e10d5177cfc782f336179820004d0
Rasmus Greve <rasmusgreve@gmail.com>
2013-11-17 01:28:16 +0100

    Added request routing

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+$show = (isset($_GET['show']) && preg_match("/[a-z]+/",$_GET['show'])) ? $_GET['show'] : 'search';
+$action = (isset($_GET['action']) && preg_match("/[a-z]+/",$_GET['action'])) ? $_GET['action'] : '';
+include("php/$show.php");
+
 
 ?>
 
@@ -13,8 +17,43 @@
 </head>
 <body>
 
+<div class="navbar navbar-inverse navbar-static-top" role="navigation">
+  <div class="container">
+	<div class="navbar-header">
+	  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+		<span class="sr-only">Toggle navigation</span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+	  </button>
+	  <a class="navbar-brand" href="./">Chords</a>
+	</div>
+	<div class="navbar-collapse collapse">
+	  <ul class="nav navbar-nav">
+		<li class="<?=($show == 'search')?'active':''?>"><a href="./?show=search">Search</a></li>
+		<li class="<?=($show == 'browse')?'active':''?>"><a href="./?show=browse">Browse</a></li>
+		<li class="<?=($show == 'create')?'active':''?>"><a href="./?show=create">New</a></li>
+	  </ul>
+	  <ul class="nav navbar-nav navbar-right">
+		<li><a href="./?action=logout">Log out</a></li>
+	  </ul>
+	</div><!--/.nav-collapse -->
+  </div>
+</div>
 
+<div class='container'>
+	<div class='row'>
+		<?php
+			getContent();
+		?>
+	</div>
+</div>
 
 <script type="text/javascript" src="./js/script.js"></script>
+<script type="text/javascript">
+	<?php
+		getJavascript();
+	?>
+</script>
 </body>
 </html>
