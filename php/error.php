2c9455b94c676fbe99121779eea2bafa551884c3
Rasmus Greve <rasmusgreve@gmail.com>
2013-11-17 02:29:41 +0100

    Added basic error handling

---
+++ php/error.php
@@ -0,0 +1,11 @@
+<?php
+function getContent()
+{
+?>
+<h1>An error occured</h1>
+<p>The page you requested could not be found!</p>
+<a href='./'>Go back to the front page</a>
+<?php
+}
+function getJavascript(){}
+?>
--- php/util.php
+++ php/util.php
@@ -9,13 +9,13 @@ function songTable($query_result)
 		</tr>
 	</thead>
 	<tbody>
-		<tr class="linkrow" href="./?show=song&id=1">
+		<tr class="linkrow" href="./?show=play&id=1">
 			<td>Fix you</td><td>Coldplay</td><td>&nbsp;<span class='label label-success'>Chords</span> <span class='label label-info'>Lyrics</span></td>
 		</tr>
-		<tr class="linkrow" href="./?show=song&id=2">
+		<tr class="linkrow" href="./?show=play&id=2">
 			<td>Samuel and Rosella</td><td>Lemon Demon</td><td>&nbsp;<span class='label label-success'>Chords</span></td>
 		</tr>
-		<tr class="linkrow" href="./?show=song&id=3">
+		<tr class="linkrow" href="./?show=play&id=3">
 			<td>Circle of Life</td><td>Elton John</td><td>&nbsp;<span class='label label-info'>Lyrics</span></td>
 		</tr>
 	</tbody>
