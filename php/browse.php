3c1ef29d5d88d4fd5e395a4c7fe0f683c34f225f
Rasmus Greve <rasmusgreve@gmail.com>
2013-11-17 02:19:51 +0100

    Moved song table to util file

--- php/browse.php
+++ php/browse.php
@@ -23,24 +23,7 @@ function getContent()
 </div>
 <div class="col-md-11">
 	<h1><?=$letter?></h1>
-	<table class="table table-striped song-table">
-		<thead>
-			<tr>
-				<th>Song title</th><th>Artist</th><th></th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr class="linkrow" href="./?show=song&id=1">
-				<td>Fix you</td><td>Coldplay</td><td>&nbsp;<span class='label label-success'>Chords</span> <span class='label label-info'>Lyrics</span></td>
-			</tr>
-			<tr class="linkrow" href="./?show=song&id=2">
-				<td>Samuel and Rosella</td><td>Lemon Demon</td><td>&nbsp;<span class='label label-success'>Chords</span></td>
-			</tr>
-			<tr class="linkrow" href="./?show=song&id=3">
-				<td>Circle of Life</td><td>Elton John</td><td>&nbsp;<span class='label label-info'>Lyrics</span></td>
-			</tr>
-		</tbody>
-	</table>
+	<?php songTable("test"); ?>
 </div>
 
 <?php
