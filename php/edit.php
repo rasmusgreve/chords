ccfcbe1a29a9fa099b6b1c89d5c3e4ef12433cbd
Rasmus Greve <rasmusgreve@gmail.com>
2013-11-18 19:42:24 +0100

    Created insertOrUpdate method for use in edit

--- php/edit.php
+++ php/edit.php
@@ -1,4 +1,18 @@
 <?php
+// http://www.youtube.com/watch?v=Wb5VOQexMBU
+$id = getPOST('id', '/-?\d+/');
+if ($id != '') //If posting an ID!
+{
+	$title = getPOST('title');
+	$artist = getPOST('artist');
+	$lyrics = getPOST('lyrics');
+	$youtube = getPOST('youtube');
+
+	$kv_pairs = array('title' => $title, 'artist' => $artist, 'lyrics' => $lyrics, 'youtube' => $youtube);
+	$id = insertOrUpdate('song',$id,$kv_pairs);
+	header("Location: ./?show=edit&id=$id");
+}
+
 //before content
 
 ?>
@@ -7,7 +21,7 @@
 function getContent()
 {
 	$id = (isset($_GET['id']) && ctype_digit($_GET['id'])) ? $_GET['id'] : -1;
-	$song = array('id' => '-1','title' => '','artist' => '','lyrics' => '');
+	$song = array('id' => '-1','title' => '','artist' => '','lyrics' => '','youtube' => '');
 	if ($id != -1)
 	{
 		$song_query = mysql_query("SELECT * FROM `song` WHERE `id` = '$id';");
@@ -16,15 +30,31 @@ function getContent()
 ?>
 <div class='col-md-12'>
 	<h1><?=($id==-1)?'Create':'Edit'?> song</h1>
-	<form class="" role="form">
-		<div class="form-group">
-			<label for="title" class="control-label">Title</label>
-				<input type="text" class="form-control" id="title" placeholder="Title" name="title" value="<?=$song['title']?>">
+	<form class="form" role="form" method="post" action="./?show=edit">
+		<input type="hidden" name="id" value="<?=$song['id']?>" />
+		<div class="row">
+			<div class="col-md-6">
+				<div class="form-group">
+					<label for="title" class="control-label">Title</label>
+					<input type="text" class="form-control" id="title" placeholder="Title" name="title" value="<?=$song['title']?>">
+				</div>
+				<div class="form-group">
+					<label for="artist" class="control-label">Artist</label>
+					<input type="text" class="form-control" id="artist" placeholder="Artist" name="artist" value="<?=$song['artist']?>">
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="form-group">
+					<label for="youtube" class="control-label">Youtube link</label>
+					<input type="text" class="form-control" id="youtube" placeholder="Youtube link" name="youtube" value="<?=$song['youtube']?>">
+				</div>
+				<div class="form-group">
+					<label for="" class="control-label">_</label>
+					<input type="text" class="form-control" id="" placeholder="" name="" value="">
+				</div>
+			</div>
 		</div>
-		<div class="form-group">
-			<label for="artist" class="control-label">Artist</label>
-				<input type="text" class="form-control" id="artist" placeholder="Artist" name="artist" value="<?=$song['artist']?>">
-		</div>
+		
 	<!-- Nav tabs -->
 	<ul class="nav nav-tabs">
 		<li class="active"><a href="#lyrics" data-toggle="tab">Lyrics</a></li>
@@ -36,7 +66,7 @@ function getContent()
 		<div class="tab-pane active" id="lyrics">
 			<div class="panel panel-default">
 				<div class="panel-body">
-					<textarea class="form-control" id="lyric_content" placeholder="Type or paste lyrics here..." rows=20><?=$song['lyrics']?></textarea>
+					<textarea class="form-control" id="lyric_content" name="lyrics" placeholder="Type or paste lyrics here..." rows=20><?=$song['lyrics']?></textarea>
 				</div>
 			</div>
 		</div>
@@ -58,7 +88,8 @@ function getContent()
 		</div>
 	</div>
 	
-	<a href="#" class="btn btn-primary">Done</a>
+	<input type="submit" class="btn btn-primary" value="Save" />
+	<a href="./" class="btn btn-default">Back</a> <!--TODO: Onclick js check for changes-->
 	
 	<!--
 	<div class="row">
--- php/util.php
+++ php/util.php
@@ -1,6 +1,64 @@
 <?php
 include_once("connectdb.php");
 
+function getGET($name, $legal_values = '', $default_value = '')
+{
+	if (is_array($legal_values))
+	{
+		return (isset($_GET[$name]) && in_array($_GET[$name],$legal_values)) ? $_GET[$name] : $default_value;
+	}
+	else if($legal_values != '')
+	{
+		return (isset($_GET[$name]) && preg_match($legal_values,$_GET[$name])) ? $_GET[$name] : $default_value;
+	}
+	else
+	{
+		return (isset($_GET[$name])) ? mysql_real_escape_string($_GET[$name]) : $default_value;
+	}
+}
+
+function getPOST($name, $legal_values = '', $default_value = '')
+{
+	if (is_array($legal_values))
+	{
+		return (isset($_POST[$name]) && in_array($_POST[$name],$legal_values)) ? $_POST[$name] : $default_value;
+	}
+	else if($legal_values != '')
+	{
+		return (isset($_POST[$name]) && preg_match($legal_values,$_POST[$name])) ? $_POST[$name] : $default_value;
+	}
+	else
+	{
+		return (isset($_POST[$name])) ? $_POST[$name] : $default_value;
+	}
+}
+
+function insertOrUpdate($table, $id, $key_values)
+{
+	if ($id == -1)
+	{
+		$keys = array(); 
+		$values = array();
+		foreach ($key_values as $key => $value)
+		{
+			$keys[] = "`$key`";
+			$values[] = "'".mysql_real_escape_string($value)."'";
+		}
+		mysql_query("INSERT INTO `$table` (" . implode(',',$keys) . ") VALUES (" . implode(',',$values) . ");");
+		return mysql_insert_id();
+	}
+	else
+	{
+		$kv_strings = array();
+		foreach ($key_values as $key => $value)
+		{
+			$kv_strings[] = "`$key` = '" . mysql_real_escape_string($value) . "'";
+		}
+		mysql_query("UPDATE `$table` SET " . implode(',',$kv_strings) . " WHERE `id` = '$id' LIMIT 1;");
+		return $id;
+	}
+}
+
 
 
 
